f3457ba8f6649401fa1672792e9c5ccf6649355c
MrD0nKey <karldesjardins13@outlook.com>
2023-02-11 17:02:42 -0500

    2/3 des fonctions dans Node fonctionne
    Je vais continuer sur Node.java, si jamais tu tiens vraiment a faire des fonctions dans cet classe la , dit le moi !

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // Vous pouvez tester votre code ici!
         Node linked = new Node(0, null);
-        linked.addValue(1);
-        System.out.println(linked.value.toString());
+        System.out.println(linked.length_recurssion());
+        linked.print();
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -11,14 +11,16 @@ public class Node {
         this.next = next;
     }
 
+    // Status : WORKS!
     public void addValue(int value) {
         if (this.next == null) {
-            this.next = new Node(value, null);
+            this.next = new Node(value);
         } else {
             this.next.addValue(value);
         }
     }
 
+    // Status : WORKS!
     public void addNode(Node next) {
         if (this.next == null) {
             this.next = next;
@@ -28,11 +30,16 @@ public class Node {
         }
     }
 
+    // Status : WORKS!
     public void removeLast() {
+        if (this.next == null) {
+            System.out.println("Un seul element dans la liste , ne peut etre enlever!");
+            return;
+        }
         if (this.next.next != null) {
             this.next.removeLast();
-        }else{
-            this.next.next = null;
+        } else {
+            this.next = null;
         }
 
         // A voir !!
@@ -45,39 +52,54 @@ public class Node {
 
     public void removeValue(int value) {
         if (this.next != null) {
-            if(this.next.value == value && this.next.next != null){
+            if (this.next.value == value && this.next.next != null) {
                 this.next = this.next.next;
-            }else{
+            } else {
                 this.next = null;
             }
         }
-        //} else if (this.next.value == value) {
-        //    while (this.next.next.value == value) {
-        //        this.next = this.next.next;
-        //    }
-        //}
-        //this.next.removeValue(value);
+        // } else if (this.next.value == value) {
+        // while (this.next.next.value == value) {
+        // this.next = this.next.next;
+        // }
+        // }
+        // this.next.removeValue(value);
 
+        // if(this.value == value){
+        // this = this.next;
+        // }
     }
 
+    // Status : WORKS!
     public int length_iteratif() {
         int length = 0;
         Node current = this;
         while (current.next != null) {
             current = current.next;
             length++;
-
         }
-        return length;
+        return length + 1;
     }
 
+    // Status : WORKS!
     public int length_recurssion() {
-        if (this.next != null) {
-            this.next = this.next.next;
-            return 1 + length_recurssion();
+        Node current = this;
+        if (current.next == null) {
+            return 1;
+        }
+        if (current.next != null) {
+            return 1 + current.next.length_recurssion();
         }
         return 0;
     }
+
+    //public int length_recurssion() {
+    //    if (this.next != null) {
+    //        this.next = this.next.next;
+    //        return 1 + length_recurssion();
+    //    }
+    //    return 0;
+    //}
 
     public int returnNlast(int nLast) {
         return 0;
@@ -89,4 +111,21 @@ public class Node {
     public void insertSort() {
     }
 
+    public void print() {
+        String word = "(";
+        Node current = this;
+        if (current.next == null) {
+            word += current.value + " -> null)";
+        } else {
+            while (current != null) {
+                if (current.next.next == null) {
+                    word += current.value + "-> " + current.next.value + "-> null )";
+                    break;
+                }
+                word += current.value + "-> ";
+                current = current.next;
+            }
+        }
+        System.out.print(word);
+    }
 }
