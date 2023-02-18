b96a102610f092349d9713927360eefe3a733b2a
Emeric Laberge <emericlaberge@gmail.com>
2023-02-18 14:21:18 -0500

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -11,16 +11,6 @@ public class Node {
         this.next = next;
     }
 
-    // Status : WORKS!
-    public void addValue(int value) {
-        if (this.next == null) {
-            this.next = new Node(value);
-        } else {
-            this.next.addValue(value);
-        }
-    }
-
-    // Status : WORKS!
     public void addNode(Node next) {
         if (this.next == null) {
             this.next = next;
@@ -30,157 +20,94 @@ public class Node {
         }
     }
 
-    // Status : WORKS!
+    /**
+     * 
+     */
     public void removeLast() {
-        if (this.next == null) {
-            System.out.println("Un seul element dans la liste , ne peut etre enlever!");
-            return;
-        }
         if (this.next.next != null) {
-            this.next.removeLast();
-        } else {
-            this.next = null;
+            this.next = this.next.next;
         }
-
-        // A voir !!
-        // } else if (this.next.next == null) {
-        // this.next = null;
-        // return;
-        // }
-        // this.next.removeLast();
     }
 
-    // Status : Works except cant remove first element
     public void removeValue(int value) {
-        if (this.value == value) {
-            this.value = this.next.value;
-            this.next = this.next.next;
-        } else {
-
-            if (this.next.next == null && this.next.value == value) {
-                this.next = null;
-            } else {
-                if (this.next.value == value) {
-                    Node nodeToRemove = this.next;
-                    this.next = this.next.next;
-                    nodeToRemove.next = null;
-                    return;
-                } else {
-                    this.next.removeValue(value);
-                }
+        if (this.next == null) {
+            return;
+        } else if (this.next.value == value) {
+            while (this.next.next.value == value) {
+                this.next = this.next.next;
             }
         }
-        // if (this.next != null) {
-        // if (this.next.value == value && this.next.next != null) {
-        // this.next = this.next.next;
-        // } else {
-        // this.next = null;
-        // }
-        // }
-        // } else if (this.next.value == value) {
-        // while (this.next.next.value == value) {
-        // this.next = this.next.next;
-        // }
-        // }
-        // this.next.removeValue(value);
+        this.next.removeValue(value);
 
-        // if(this.value == value){
-        // this = this.next;
-        // }
+        // TODO
     }
 
-    // Status : WORKS!
     public int length_iteratif() {
         int length = 0;
-        Node current = this;
-        while (current.next != null) {
-            current = current.next;
+        while (this.next != null) {
+            this.next = this.next.next;
             length++;
+
         }
-        return length + 1;
+        // TODO
+        return length;
     }
 
-    // Status : WORKS!
     public int length_recurssion() {
-        Node current = this;
-        if (current.next == null) {
-            return 1;
+        if (this.next != null) {
+            this.next = this.next.next;
+            return 1 + length_recurssion();
         }
-        if (current.next != null) {
-            return 1 + current.next.length_recurssion();
-        }
+        return 0;
+        // TODO
+    }
+
+    int returnNlast(int nLast) {
+        // TODO
         return 0;
     }
 
-    // public int length_recurssion() {
-    // if (this.next != null) {
-    // this.next = this.next.next;
-    // return 1 + length_recurssion();
-    // }
-    // return 0;
-    // }
-
-    // Status : WORKS !
-    public int returnNlast(int nLast) {
-        Node first = this;
-        Node current = this;
-        int compteur = 1;
-        while (current.value != nLast && current.next != null) {
-            current = current.next;
-            ++compteur;
-        }
-        return first.length_recurssion() - compteur + 1;
-    }
-
-    // Status : Works !!
     public void addValue_ordered(int value) {
-        int nodeValue = this.value;
-        if (value < nodeValue) {
-            int tmp = nodeValue;
-            this.value = value;
-            value = tmp;
-            this.next = new Node(value, this.next);
-            return;
-        } else {
-            if (this.next == null) {
-                this.next = new Node(value, null);
-            } else {
-                this.next.addValue_ordered(value);
-            }
-        }
+        // TODO
     }
 
     public void insertSort() {
-        if (this.next != null) {
-            Node nodepremier = this;
-            Node current = this.next;
-            nodepremier.next = null;
-            while (current.next != null) {
-                nodepremier.addValue_ordered(current.value);
-                current = current.next;
-            }
-            if(current.next == null){
-                nodepremier.addValue_ordered(current.value);
-            }
+        if (this.next == null) {
+            return;
         }
+        Node sortedTail = this; // dernier element trié
+        Node unsorted = this.next; // élément à trier
+        sortedTail.next = null; // Initialement seul le head est sorted
+
+        while (unsorted != null) {
+            Node nextUnsorted = unsorted.next;
+            if (unsorted.value < sortedTail.value) {
+                unsorted.next = sortedTail; // on
+                sortedTail = unsorted;
+            } else {
+                Node prev = null;
+                Node curr = sortedTail;
+                while (curr != null && curr.value < unsorted.value) {
+                    prev = curr;
+                    curr = curr.next;
+                }
+                if (prev != null) {
+                    prev.next = unsorted;
+                    unsorted.next = curr;
+                } else {
+                    unsorted.next = sortedTail;
+                    sortedTail = unsorted;
+                }
+            }
+            unsorted = nextUnsorted;
+        }
+
+        this.value = sortedTail.value;
+        this.next = sortedTail.next;
     }
 
-    // Une fonction pour voir la linked list.
-    public void print() {
-        String word = "(";
-        Node current = this;
-        if (current.next == null) {
-            word += current.value + " -> null)";
-        } else {
-            while (current != null) {
-                if (current.next.next == null) {
-                    word += current.value + "-> " + current.next.value + "-> null )";
-                    break;
-                }
-                word += current.value + "-> ";
-                current = current.next;
-            }
-        }
-        System.out.print(word);
+    public int getValue() {
+        return this.value;
     }
+
 }
